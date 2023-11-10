1bd02e62e3064fb4a6d2557cb3253beb2d8816b0
Alexander Klokov <alexander.klokov@gmail.com>
2023-11-10 16:13:08 -0600

    move data to the storage

--- src/mc_train.py
+++ src/mc_train.py
@@ -1,7 +1,9 @@
+import os
+
 from mc import MarkovChain
 from mc_utils import load_data
 
-directory_path = 'data'
+directory_path = os.environ["DATA_TEXT_ESENIN"]
 
 data = load_data(directory_path)
 
